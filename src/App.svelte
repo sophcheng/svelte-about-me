74c236c4838b3c16d3c80e901cdcf6f1e092df26
sophcheng <sophiecheng181@gmail.com>
2025-07-15 12:41:28 -0400

    Redeploying image path fix

--- src/App.svelte
+++ src/App.svelte
@@ -9,19 +9,19 @@
 
   let cardData = [
     {
-      image: "/svelte-about-me/assets/aiCat 1.png",
+      image: "/assets/aiCat 1.png",
       title: "Sophware",
       description: "(Software). I've built Swift-based apps and websites!",
       color: "#3AA6B9",
     },
     {
-      image: "/svelte-about-me/assets/rumi.png",
+      image: "/assets/rumi.png",
       title: "Drawing",
       description: "I'm familiar with digital, traditional, and doodling!",
       color: "#FFD0D0",
     },
     {
-      image: "/svelte-about-me/assets/iconAlt.png",
+      image: "/assets/iconAlt.png",
       title: "Language",
       description:
         "I like learning new languages -- of people, 'puters, puns, and patterns.",
@@ -31,19 +31,19 @@
 
   let funData = [
     {
-      image: "/svelte-about-me/assets/meowfficial.png",
+      image: "/assets/meowfficial.png",
       title: "Meow",
       description:
         "I once built an app demonstrating the effects of AI on our cultural and physical climate using cats.",
     },
     {
-      image: "/svelte-about-me/assets/icon.png",
+      image: "/assets/icon.png",
       title: "Misfortune",
       description:
         "I stayed overnight at a jail once, because there were no vacant hotels that night (due to a local poker competition).",
     },
     {
-      image: "/svelte-about-me/assets/jinu.png",
+      image: "/assets/jinu.png",
       title: "Muse",
       description: "I am in K-Pop Demon Hunters jail right now. #FREEJINU",
     },
